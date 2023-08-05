e9a046f046c57a2387e24aa9779d2a6c5aa0cbcf
Jesse Sorman <jessesorman@gmail.com>
2023-08-05 11:09:37 -0400

    add additional testing to recipe request

--- spec/requests/api/v1/recipe_request_spec.rb
+++ spec/requests/api/v1/recipe_request_spec.rb
@@ -3,11 +3,15 @@ require "rails_helper"
 RSpec.describe "Recipe API request" do
   describe "API request" do
     it "can request recipes based on country", :vcr do
-      query_params = {country: "United States of America"}
+      query_params = {country: "American"}
       get api_v1_recipes_path, params: query_params
       expect(response).to be_successful
-      recipe = JSON.parse(response.body, symbolize_names: true)
-      expect(recipe).to be_a(Array)
+      recipes = JSON.parse(response.body, symbolize_names: true)
+      expect(recipes).to be_a(Array)
+      expect(recipes[0][:name]).to eq("Frothy Iced Matcha Green Tea Recipe")
+      expect(recipes[0][:country]).to eq(["american"])
+      expect(recipes[0][:image]).to be_a(String)
+      expect(recipes[0][:url]).to be_a(String)
     end
   end
 end
