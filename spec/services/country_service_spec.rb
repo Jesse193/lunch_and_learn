492cd5f383bc691e07cf912e8cd001e975830a3d
Jesse Sorman <jessesorman@gmail.com>
2023-08-05 14:59:55 -0400

    add testing for country service to not return

--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -11,7 +11,9 @@ describe CountryService do
         expect(country[0][:capital]).to be_a Array
         expect(country[0][:capital]).to eq(["Washington, D.C."])
         expect(country[0][:demonyms][:eng][:m]).to eq("American")
-        expect(country[0][:pie]).to eq(nil)
+        expect(country[0]).to_not have_key(:pie)
+        expect(country[0]).to_not have_key(:apples)
+        expect(country[0]).to_not have_key(:cities)
       end
     end
   end
