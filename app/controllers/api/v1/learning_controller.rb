72f7ce5fcdc7db752866026627e4f12f812948e2
Jesse Sorman <jessesorman@gmail.com>
2023-08-05 19:59:40 -0400

    add testing for learning request

--- app/controllers/api/v1/learning_controller.rb
+++ app/controllers/api/v1/learning_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::LearningController < ApplicationController
   def index
     country = CountryFacade.new.country(params[:country])
-    learning = LearningFacade.new.videos(country)
+    learning = LearningFacade.new.videos(country[0].name)
     render json: learning
   end
 end
---
+++ spec/requests/api/v1/learning_request_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "Learning video API request" do
+  describe "API request" do
+    it "can request videos based on country", :vcr do
+      query_params = {country: "Canada" }
+      get  api_v1_learning_index_path, params: query_params
+      expect(response).to be_successful
+      videos = JSON.parse(response.body, symbolize_names: true)
+      expect(videos[0]).to have_key(:title)
+      expect(videos[0]).to have_key(:youtube_video_id)
+      expect(videos[0][:title]).to eq("A Super Quick History of Canada")
+      expect(videos[0][:youtube_video_id]).to eq("e_okM7Mig6s")
+    end
+  end
+end
