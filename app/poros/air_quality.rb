aeca4a677c5b44cde9cd557856021ba4538410f0
Jesse Sorman <jessesorman@gmail.com>
2023-08-07 11:40:38 -0400

    add additional testing to air quality facade

--- app/poros/air_quality.rb
+++ app/poros/air_quality.rb
@@ -1,7 +1,7 @@
 class AirQuality
-  attr_reader :air, :pm25_concentration, :co_concentration
+  attr_reader :aqi, :pm25_concentration, :co_concentration
   def initialize(attributes)
-    @air = attributes[:overall_aqi]
+    @aqi = attributes[:overall_aqi]
     @pm25_concentration = attributes[:"PM2.5"][:concentration]
     @co_concentration = attributes[:CO][:concentration]
   end
--- spec/facades/air_quality_facade.rb
+++ spec/facades/air_quality_facade.rb
@@ -5,7 +5,12 @@ RSpec.describe AirQualityFacade, type: :model do
     it "can return air quality by capital", :vcr do 
       capital = "Paris"
       air_quality = AirQualityFacade.new.air_quality(capital)
-      expect(air_quality).to be_a(Array)
+      expect(air_quality.aqi).to be_a(Integer)
+      expect(air_quality.pm25_concentration).to be_a(Float)
+      expect(air_quality.co_concentration).to be_a(Float)
+      expect(air_quality.co_concentration).to_not be_a(String)
+      expect(air_quality.aqi).to_not be_a(String)
+      expect(air_quality.pm25_concentration).to_not be_a(String)
     end
   end
 end
