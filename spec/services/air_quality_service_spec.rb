7fdfe6d9be7b4d270647a0198a8548855594a7d6
Jesse Sorman <jessesorman@gmail.com>
2023-08-07 11:19:19 -0400

    add additional testing to air quality service testing

--- spec/services/air_quality_service_spec.rb
+++ spec/services/air_quality_service_spec.rb
@@ -5,9 +5,21 @@ describe AirQualityService do
     context "#air_quality" do
       it "returns air quality of a capital", :vcr do
         capital = "Paris"
-        capital = AirQualityService.new.air_quality(capital)
-        expect(capital).to be_a(Hash)
-        
+        air_quality = AirQualityService.new.air_quality(capital)
+        expect(air_quality).to be_a(Hash)
+        expect(air_quality[:CO]).to have_key(:concentration)
+        expect(air_quality[:CO]).to have_key(:aqi)
+        expect(air_quality[:NO2]).to have_key(:concentration)
+        expect(air_quality[:NO2]).to have_key(:aqi)
+        expect(air_quality[:O3]).to have_key(:concentration)
+        expect(air_quality[:SO2]).to have_key(:concentration)
+        expect(air_quality[:SO2]).to have_key(:aqi)
+        expect(air_quality[:"PM2.5"]).to have_key(:concentration)
+        expect(air_quality[:"PM2.5"]).to have_key(:aqi)
+        expect(air_quality[:PM10]).to have_key(:concentration)
+        expect(air_quality[:PM10]).to have_key(:aqi)
+        expect(air_quality).to have_key(:overall_aqi)
+        expect(air_quality).to_not have_key(:puppies)
       end
     end
   end
