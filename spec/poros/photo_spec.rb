bfdd0c79f2e8266804f2c942b57cd9592e189335
Jesse Sorman <jessesorman@gmail.com>
2023-08-08 11:46:46 -0400

    add photo poro testing

---
+++ spec/poros/photo_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe Photo, type: :model do
+  describe "creates Photo object" do
+    it "has attributes" do
+      data = {
+        description: "Statue of liberty",
+        urls: {
+          regular: "APhotoLink"
+        }
+      }
+      photo = Photo.new(data)
+      expect(photo).to be_a(Photo)
+      expect(photo.alt_tag).to eq("Statue of liberty")
+      expect(photo.alt_tag).to be_a(String)
+      expect(photo.url).to eq("APhotoLink")
+      expect(photo.url).to be_a(String)
+    end
+  end
+end
